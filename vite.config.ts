ceaa6bc9c73927e13111a8b7b7a180d8eb2e228f
Ivan Chen <ivanchenhz@gmail.com>
2021-06-15 13:09:08 +0800

    feat(types): Add types,

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,12 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import dts from 'vite-plugin-dts'
 
 const path = require('path')
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()],
+  plugins: [vue(), dts()],
   build: {
     lib: {
       entry: path.resolve(__dirname, 'src/index.ts'),
